b0e26ef2f965d282a33c5e10961209a8eee85570
Daniel Marinho <marinhoadm1986@gmail.com>
2023-09-23 19:16:01 -0300

    Criação de blog e components

---
+++ src/app/components/smal-card/smal-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smal-card',
+  templateUrl: './smal-card.component.html',
+  styleUrls: ['./smal-card.component.css']
+})
+export class SmalCardComponent {
+
+}
